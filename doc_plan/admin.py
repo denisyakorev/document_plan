be47553151899ca10c2cb5b31215d21d52b36a1e
Denis Yakorev <denisyakorev@mail.ru>
2018-01-27 18:28:11 +0300

    adding paginator

--- doc_plan/admin.py
+++ doc_plan/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from doc_plan.models import Project
+
 
 # Register your models here.
+admin.site.register(Project)
--- doc_plan/views.py
+++ doc_plan/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.template import loader, RequestContext
 from django.views.generic.list import ListView
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 
 from doc_plan.models import Project
@@ -10,10 +11,24 @@ class ProjectListView(ListView):
     model = Project
     context_object_name = 'projects'
     template_name = 'projects.html'
+    paginate_by = 2
 
 
     def get_queryset(self):
-        qs = Project.objects.filter(created_by=self.request.user)
-        return qs
+        queryset = Project.objects.filter(created_by=self.request.user)
+        '''
+        print(len(qs))
+        paginator = Paginator(qs, 2)
+        page = self.request.GET.get('page')
+        try:
+            qs = paginator.get_page(page)
+        except PageNotAnInteger:
+            # If page is not an integer, deliver first page.
+            qs = paginator.get_page(1)
+        except EmptyPage:
+            # If page is out of range (e.g. 9999), deliver last page of results.
+            qs = paginator.get_page(paginator.num_pages)
+        '''
+        return queryset
 
 
