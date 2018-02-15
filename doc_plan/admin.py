243a22ac20044fc1940ec25dc3fe915a8fcdb9e8
linux17 <denisyakorev@mail.ru>
2018-02-15 08:32:58 -0800

    make some changes in admin interface

--- doc_plan/admin.py
+++ doc_plan/admin.py
@@ -1,7 +1,19 @@
 from django.contrib import admin
 from doc_plan.models import Project, Chapter
+from django.utils.translation import ugettext_lazy as _
 
 
 # Register your models here.
-admin.site.register(Project)
+
+class ProjectAdmin(admin.ModelAdmin):
+	fieldsets = [
+		(None, 				 	{'fields': ['name']}),
+		(_('Задачи проекта'), 	{'fields': ['aim_action', 'aim_auditory', 'aim_content', 'reaction_action', 'reaction_standart']}),
+		(_('Аудитория'), 		{'fields': ['auditory_duty', 'auditory_knowledge', 'auditory_demography', 'auditory_relations', 'auditory_environment', 'auditory_resume']}),
+		(_('Вопросы'), 			{'fields': ['question_actions', 'question_knowledges']}),
+		(_('Разделы'), 			{'fields': ['chapters']}),
+	]
+
+
+admin.site.register(Project, ProjectAdmin)
 admin.site.register(Chapter)
--- doc_plan/models.py
+++ doc_plan/models.py
@@ -5,43 +5,49 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Project(models.Model):
-    name = models.CharField(max_length=200, verbose_name=_("project_name"))
-    aim_action = models.TextField(blank=True, verbose_name=_("aim_action"))
-    aim_auditory = models.TextField(blank=True, verbose_name=_("aim_auditory"))
-    aim_content = models.TextField(blank=True, verbose_name=_("aim_content"))
-    reaction_action = models.TextField(blank=True, verbose_name=_("reaction_action"))
-    reaction_standart = models.TextField(blank=True, verbose_name=_("reaction_standart"))
-    auditory_duty = models.TextField(blank=True, verbose_name=_("auditory_duty"))
-    auditory_knowledge = models.TextField(blank=True, verbose_name=_("auditory_knowledge"))
-    auditory_demography = models.TextField(blank=True, verbose_name=_("auditory_demography"))
-    auditory_relations = models.TextField(blank=True, verbose_name=_("auditory_relations"))
-    auditory_environment = models.TextField(blank=True, verbose_name=_("auditory_environment"))
-    auditory_resume = models.TextField(blank=True, verbose_name=_("auditory_resume"))
-    question_actions = models.TextField(blank=True, verbose_name=_("question_actions"))
-    question_knowledges = models.TextField(blank=True, verbose_name=_("question_knowledges"))
-    chapters = models.ManyToManyField("Chapter", blank=True, verbose_name=_("chapters"))
+    name = models.CharField(max_length=200, verbose_name=_("название проекта"))
+    aim_action = models.TextField(blank=True, verbose_name=_("действие"))
+    aim_auditory = models.TextField(blank=True, verbose_name=_("аудитория"))
+    aim_content = models.TextField(blank=True, verbose_name=_("содержание"))
+    reaction_action = models.TextField(blank=True, verbose_name=_("реакция"))
+    reaction_standart = models.TextField(blank=True, verbose_name=_("стандарт"))
+    auditory_duty = models.TextField(blank=True, verbose_name=_("обязанности"))
+    auditory_knowledge = models.TextField(blank=True, verbose_name=_("знания"))
+    auditory_demography = models.TextField(blank=True, verbose_name=_("демография"))
+    auditory_relations = models.TextField(blank=True, verbose_name=_("отношения"))
+    auditory_environment = models.TextField(blank=True, verbose_name=_("окружение"))
+    auditory_resume = models.TextField(blank=True, verbose_name=_("выводы"))
+    question_actions = models.TextField(blank=True, verbose_name=_("процедуры"))
+    question_knowledges = models.TextField(blank=True, verbose_name=_("знания"))
+    chapters = models.ManyToManyField("Chapter", blank=True, verbose_name=_("разделы"))
     created_at = models.DateField(auto_now_add=True)
     created_by = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __unicode__(self):
         return self.name
 
+    def __str__(self):
+        return self.name
+
     class Meta:
-        verbose_name = _("project")
-        verbose_name_plural = _("projects")
+        verbose_name = _("проект")
+        verbose_name_plural = _("проекты")
         ordering = ["-created_at"]
 
 
 class Chapter(models.Model):
-    name = models.CharField(max_length=200, verbose_name=_("chapter_name"))
-    questions = models.TextField(blank=True, verbose_name=_("questions"))
+    name = models.CharField(max_length=200, verbose_name=_("название"))
+    questions = models.TextField(blank=True, verbose_name=_("вопросы"))
 
     def __unicode__(self):
         return self.name
 
+    def __str__(self):
+        return self.name
+
     class Meta:
-        verbose_name = _("chapter")
-        verbose_name_plural = _("chapters")
+        verbose_name = _("раздел")
+        verbose_name_plural = _("разделы")
 
 
 
